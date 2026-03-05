a157b7176c6fed79b67c731ae479a4a5c683e2ef
Kevin Spears <kevin@thespears.com>
2026-03-04 17:59:55 -0800

    fix: use CartoDB Voyager tiles to show English place names on map

--- src/components/Observation/PlaceMap.tsx
+++ src/components/Observation/PlaceMap.tsx
@@ -60,8 +60,8 @@ export function PlaceMap({ places }: PlaceMapProps) {
         scrollWheelZoom={true}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
         <FitBounds places={mappable} />
         {mappable.map(place => (
